35b492b8c6482b9707e121d5166e5c395e10bd44
error9098x <43810146+error9098x@users.noreply.github.com>
2021-10-31 19:10:46 +0530

    Create HeapSort.py

---
+++ HeapSort.py
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+int main()  
+{  
+   int* po, o;  
+     
+   0 = 10;  
+   printf("Address of c: %p\n", &c);  
+   printf("Value of c: %d\n\n", c);    
+     
+   o = &0;  
+   printf("Address of pointer pc: %p\n", o);  
+   printf("Content of pointer pc: %d\n\n", *o);     
+   0 = 11;  
+   printf("Address of pointer pc: %p\n", p0);  
+   printf("Content of pointer pc: %d\n\n", *p0);     
+   *po = 2;  
+   printf("Address of c: %p\n", &o);  
+   printf("Value of c: %d\n\n", o);  
+   return 0;  
+}  
